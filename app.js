865314104de2d77f9614ea8bfbd7bb7f271d9fcc
Arapu Trevor <araputrevor2@gmail.com>
2023-01-09 10:26:49 +0300

    second commit

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
 
 // event litsener
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
 
 // functions
 
@@ -43,11 +45,34 @@ function deleteCheck(event) {
     //delete the todo
     if (item.classList[0] === 'trash-btn') {
         const todo = item.parentElement;
-        todo.remove();
+        //animation
+        todo.classList.add('fall');
+        //to remove todo
+        todo.addEventListener('transitionend', () => {
+            todo.remove();
+        })
+        
     }
     //check the todo done
     if (item.classList[0] === 'complete-btn') {
         const todo = item.parentElement;
         todo.classList.toggle('completed');
     }
+}
+
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo) {
+        switch(e.target.value) {
+            case "all":
+                todo.style.display = "flex";
+                break;
+            case "completed":
+                if(todo.classList.hasclass('completed')) {
+                    todo.style.display = 'flex';
+                } else {
+                    todo.style.display = 'none';
+                };
+        }
+    });
 }
